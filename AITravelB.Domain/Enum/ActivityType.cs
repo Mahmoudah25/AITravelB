ce09238b5950587a60efa246381e65f66d9c9ab4
MahmoudAhm25 <mahmoudandelsamed@gmail.com>
2026-08-03 05:37:19 +0300

    feat: add Trip, Activity entities and Budget value object (TICKET-101)

---
+++ AITravelB.Domain/Enum/ActivityType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AITravelB.Domain.Enum
+{
+    internal class ActivityType
+    {
+    }
+}

f3f15f2813ef3eea447a0daa52e197348c9fecc8
MahmoudAhm25 <mahmoudandelsamed@gmail.com>
2026-08-04 21:17:54 +0300

    add GetTripById query and endpoint (TICKET-103)

--- AITravelB.API/Controllers/TripsController.cs
+++ AITravelB.API/Controllers/TripsController.cs
@@ -1,4 +1,5 @@
 ﻿using AITravelB.Application.Trips.Commands;
+using AITravelB.Application.Trips.Queries.GrtTripById;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,17 @@ namespace AITravelB.API.Controllers
         {
             var tripId = await mediator.Send(command);
             return Ok(tripId);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTripById(Guid id)
+        {
+            var trip = await mediator.Send(new GetTripByIdQuery(id));
+            if (trip == null)
+            {
+                return NotFound();
+            }
+            return Ok(trip);
         }
 
     }
